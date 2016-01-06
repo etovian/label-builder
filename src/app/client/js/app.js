ee955a1fd4cefab75f91905bba265335921b6135
Mike Green <mike.green@stgconsulting.com>
2016-01-05 20:54:56 -0700

    LB-158, LB-159

--- src/app/client/js/app.js
+++ src/app/client/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-	angular.module('app', ['ngRoute', 'templates', 'ui.bootstrap', 'ngAside']);
+	angular.module('app', ['ngRoute', 'templates', 'ui.bootstrap', 'ngAside', 'ui.grid', 'ui.grid.selection']);
 	angular.module('templates', []); //necessary for now, because gulp-angular-templatecache won't put them in 'app'
 })();
 
--- src/app/client/js/controllers/label.list.controller.js
+++ src/app/client/js/controllers/label.list.controller.js
@@ -10,12 +10,59 @@
 
 		var vm = this;
 		angular.extend(vm, {
-
+			gridOptions: {
+				columnDefs: [ //https://github.com/angular-ui/ui-grid/wiki/Defining-columns
+					{
+						displayName: 'Item Code',
+						field: 'itemCode'
+					}, {
+						displayName: 'Description',
+						field: 'productName'
+					}, {
+						displayName: 'Brand',
+						field: 'brandCode'
+					}, {
+						displayName: 'version',
+						field: 'versionMajor'
+					}, {
+						displayName: 'Status',
+						field: 'status'
+					}, {
+						displayName: 'Owner',
+						field: 'owner'
+					}, {
+						displayName: 'Modified',
+						field: 'lastModified'
+					}, {
+						displayName: 'By',
+						field: 'modifier'
+					}, {
+						displayName: 'Launch',
+						field: 'launched'
+					}
+				],
+				data: [],
+				enableRowSelection: true,
+				enableRowHeaderSelection: false,
+				multiSelect: false,
+				noUnselect: true,
+				onRegisterApi: function(gridApi) {
+					vm.gridApi = gridApi;
+					gridApi.selection.on.rowSelectionChanged(null, function(row) {
+						vm.setSelectedLabel(row.entity);
+					});
+				}
+			},
 			getExistingLabelSummaries: function() {
 				return labelService.getExistingLabelSummaries();
+			},
+			setSelectedLabel: function(label) {
+				console.log(label);
 			}
 		});
 
-		labelService.requestExistingLabelSummaries();
+		labelService.requestExistingLabelSummaries().then(function() {
+			vm.gridOptions.data = labelService.getExistingLabelSummaries();
+		});
 	}
 })();
